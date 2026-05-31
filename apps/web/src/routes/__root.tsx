aa95b97853712e0e58fa8874fb88c505dcbb1ea4
Hunchojoe19 <enubiakjoseph@gmail.com>
2026-05-31 22:22:42 +0100

    style: standardize layout margins by removing sportsbook-specific conditional spacing

--- apps/web/src/routes/__root.tsx
+++ apps/web/src/routes/__root.tsx
@@ -184,10 +184,7 @@ arguments])}}var i,s,r=w[b],z=" ",l="init options track screen onReady".split(z)
 											<main className="no-scrollbar flex-1 overflow-y-auto">
 												<div
 													className={cn(
-														"grid py-4 md:gap-8",
-														isSportsbookRoute
-															? "mx-2 lg:mx-4"
-															: "mx-4 lg:mx-[104px]",
+														"grid py-4 md:gap-8 mx-4 lg:mx-[104px]",
 														shouldShowSidebar
 															? "lg:grid-cols-[20%_80%]"
 															: "lg:grid-cols-1",
