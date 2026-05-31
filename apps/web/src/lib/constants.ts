51a620397e41795cec20ac34535dd731b3bba548
Hunchojoe19 <enubiakjoseph@gmail.com>
2026-05-31 21:58:16 +0100

    refactor: add boxing and UFC to sports constants, implement sticky right sidebars, and include new sport logo components

--- apps/web/src/lib/constants.ts
+++ apps/web/src/lib/constants.ts
@@ -2,6 +2,8 @@ export const SPORTS = {
 	FOOTBALL: "football",
 	TENNIS: "tennis",
 	BASKETBALL: "basketball",
+	BOXING: "boxing",
+	UFC: "ufc",
 } as const;
 
 export type Sport = (typeof SPORTS)[keyof typeof SPORTS];
