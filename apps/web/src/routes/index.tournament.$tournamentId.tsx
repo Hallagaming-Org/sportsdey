cee2be62de6531c9cfef37a18060f4bc32e5ddb3
Hunchojoe19 <enubiakjoseph@gmail.com>
2026-05-31 21:58:58 +0100

    style: make right sidebar sticky on desktop layouts in tournament view

--- apps/web/src/routes/index.tournament.$tournamentId.tsx
+++ apps/web/src/routes/index.tournament.$tournamentId.tsx
@@ -215,7 +215,7 @@ function RouteComponent() {
 						</AccordionItem>
 					</Accordion>
 				</div>
-				<div className="no-scrollbar hidden h-full overflow-y-auto pb-20 lg:block">
+				<div className="no-scrollbar hidden h-full overflow-y-auto pb-20 lg:block lg:sticky lg:top-4 lg:max-h-[calc(100vh-2rem)]">
 					<RightSidebar />
 				</div>
 			</div>
