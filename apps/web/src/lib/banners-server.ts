05b820c08cd6ef178068073492e588fc0e3635a2
adnatecodez <adeaduratimilehin@gmail.com>
2026-06-03 13:50:55 +0100

    made a fix for banner to fetch from api instead.

--- apps/web/src/lib/banners-server.ts
+++ apps/web/src/lib/banners-server.ts
@@ -1,27 +1,26 @@
-import { createServerFn } from "@tanstack/react-start";
-import { client, urlFor } from "./sanity";
+import { apiRequest } from "./api";
 
-const SANITY_TIMEOUT = 5000;
+// const SANITY_TIMEOUT = 5000;
 
-async function fetchWithSanityTimeout<T>(query: string): Promise<T> {
-	const controller = new AbortController();
-	const timeoutId = setTimeout(() => controller.abort(), SANITY_TIMEOUT);
+// async function fetchWithSanityTimeout<T>(query: string): Promise<T> {
+// 	const controller = new AbortController();
+// 	const timeoutId = setTimeout(() => controller.abort(), SANITY_TIMEOUT);
 
-	try {
-		const result = await client.fetch<T>(query, {}, {
-			signal: controller.signal,
-		});
-		clearTimeout(timeoutId);
-		return result;
-	} catch (error) {
-		clearTimeout(timeoutId);
-		if (error instanceof Error && error.name === "AbortError") {
-			console.warn("Sanity API request timed out");
-			throw new Error("Sanity request timed out");
-		}
-		throw error;
-	}
-}
+// 	try {
+// 		const result = await client.fetch<T>(query, {}, {
+// 			signal: controller.signal,
+// 		});
+// 		clearTimeout(timeoutId);
+// 		return result;
+// 	} catch (error) {
+// 		clearTimeout(timeoutId);
+// 		if (error instanceof Error && error.name === "AbortError") {
+// 			console.warn("Sanity API request timed out");
+// 			throw new Error("Sanity request timed out");
+// 		}
+// 		throw error;
+// 	}
+// }
 
 export interface BannerData {
 	_id: string;
