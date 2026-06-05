c99e56bf2299e2a2b57eef1f708ee1af7d586ada
Hunchojoe19 <enubiakjoseph@gmail.com>
2026-06-05 04:46:42 +0100

    style: enforce dark theme globally and remove theme toggle component

--- apps/web/src/routes/auth/_layout.tsx
+++ apps/web/src/routes/auth/_layout.tsx
@@ -3,7 +3,7 @@ import { ThemeProvider } from "@/components/theme-provider";
 
 export const Route = createFileRoute("/auth/_layout")({
 	component: () => (
-		<ThemeProvider attribute="class" defaultTheme="light" enableSystem>
+		<ThemeProvider attribute="class" defaultTheme="dark" forcedTheme="dark">
 			<div className="flex min-h-screen flex-col">
 				<main className="flex-1">
 					<Outlet />
