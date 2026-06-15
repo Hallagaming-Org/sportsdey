f7c221bdb3a022b91316bb940c75c8f25ae211d4
adnatecodez <adeaduratimilehin@gmail.com>
2026-06-15 16:43:16 +0100

    fixing migration issues.

--- apps/server/src/db/migrations/0013_military_doomsday.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE `wallet_transaction` ADD `recipient_wallet_id` text;--> statement-breakpoint
-ALTER TABLE `wallet_transaction` ADD `recipient_name` text;--> statement-breakpoint
-ALTER TABLE `game` DROP COLUMN `category`;
