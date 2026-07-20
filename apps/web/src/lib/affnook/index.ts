4428f819464c573c2880f5976dea856469076d33
Nathaniel Essien <thaniessy@gmail.com>
2026-07-20 15:10:46 +0100

    feat: create index for Affnook service exports
    - Added an index file to streamline exports for Affnook attribution and synchronization functions.
    - Included relevant types for improved type safety in module interactions.

---
+++ apps/web/src/lib/affnook/index.ts
@@ -0,0 +1,11 @@
+export {
+	captureAffnookAttributionFromUrl,
+	clearAffnookAttribution,
+	getAffnookAttribution,
+	type AffnookAttribution,
+} from "./attribution";
+export {
+	resolveAffnookAuthEvent,
+	syncAffnookAuthEvent,
+	type AffnookSyncEvent,
+} from "./sync";
