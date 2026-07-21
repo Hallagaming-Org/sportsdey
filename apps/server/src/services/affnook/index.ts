615a3e42ac350981d038507c2c8080a3d8fc52db
Nathaniel Essien <thaniessy@gmail.com>
2026-07-21 11:53:55 +0100

    refactor: remove unused queue function and types from Affnook customer service
    - Eliminated the queueAffnookCustomerSync function and associated types AffnookUserLike and AffnookSyncOptions to streamline the customer service module.
    - This change contributes to cleaner code and improved maintainability.

--- apps/server/src/services/affnook/index.ts
+++ apps/server/src/services/affnook/index.ts
@@ -1,10 +1,7 @@
 export { getAffnookConfig, isAffnookConfigured } from "./config";
 export {
 	createAffnookCustomer,
-	queueAffnookCustomerSync,
 	recordAffnookCustomerLogin,
-	type AffnookSyncOptions,
-	type AffnookUserLike,
 } from "./customers.service";
 export type {
 	AffnookApiResult,
