895025c1e00c14c3fad28d63f4b5847a32538f20
adnatecodez <adeaduratimilehin@gmail.com>
2026-08-05 18:03:48 +0100

    fixed migration issue drifting because of different branches doing contribution to it
    Signed-off-by: adnatecodez <adeaduratimilehin@gmail.com>

--- apps/server/src/db/migrations/0027_bonus_engine_game_providers.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE `game` ADD `provider_id` text;--> statement-breakpoint
-ALTER TABLE `game` ADD `provider_name` text;--> statement-breakpoint
-ALTER TABLE `game` ADD `is_live_game` integer DEFAULT false NOT NULL;--> statement-breakpoint
-ALTER TABLE `game` ADD `free_spin` integer DEFAULT false NOT NULL;
--- apps/server/src/db/migrations/0028_cute_blizzard.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `sportsbook_session` (
-	`id` text PRIMARY KEY NOT NULL,
-	`user_id` text NOT NULL,
-	`created_at` integer DEFAULT (cast(unixepoch('subsecond') * 1000 as integer)) NOT NULL,
-	`updated_at` integer DEFAULT (cast(unixepoch('subsecond') * 1000 as integer)) NOT NULL,
-	FOREIGN KEY (`user_id`) REFERENCES `user`(`id`) ON UPDATE no action ON DELETE cascade
-);
