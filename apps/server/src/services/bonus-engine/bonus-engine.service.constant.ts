7642351a49af0ca2adb8be682f77a4c5e27c771c
Nathaniel Essien <thaniessy@gmail.com>
2026-09-22 12:51:12 +0100

    feat: add tournament-related constants to bonus engine service
    Expanded the BONUS_ENGINE_PATH and BONUS_ENGINE_BODY_FIELD constants to include new endpoints and fields for tournament functionalities, such as listing tournaments, joining tournaments, and accessing tournament leaderboards. This update enhances the server's integration with tournament features in the Bonus Engine.

--- apps/server/src/services/bonus-engine/bonus-engine.service.constant.ts
+++ apps/server/src/services/bonus-engine/bonus-engine.service.constant.ts
@@ -13,6 +13,9 @@ export const BONUS_ENGINE_PATH = {
 	LOYALTY_LISTS: "/loyalty/lists",
 	MISSION_LIST: "/mission/list",
 	MISSION_PROGRESS: "/mission/progress",
+	TOURNAMENT_LIST: "/tournament/list",
+	TOURNAMENT_JOIN: "/tournament/join",
+	TOURNAMENT_LEADERBOARD: "/tournament/leaderboard",
 	LIST_ACTIVE_CAMPAIGN: "/list_active_campaign",
 	GETALL_USER_BONUS: "/getall_User_bonus",
 	ACTIVATE_BONUS: "/activate_bonus",
@@ -198,6 +201,8 @@ export const BONUS_ENGINE_BODY_FIELD = {
 	IS_ROLLBACK: "isRollback",
 	RESULT_TIME: "result_time",
 	MISSION_ID: "mission_id",
+	TOURNAMENT_ID: "tournament_id",
+	TOURNAMENT_ID_JOIN: "tournamentId",
 } as const;
 
 export const BONUS_ENGINE_REPORT_RETRY_ATTEMPTS = 3;
